2005597106efb2d8f4d2c95c0406b67acef565d8
nakamura-to <toshihiro.nakamura@gmail.com>
2014-03-19 17:03:56 +0900

    @ Version を付与し忘れたのを修正しました

--- src/main/java/boilerplate/entity/Employee.java
+++ src/main/java/boilerplate/entity/Employee.java
@@ -5,6 +5,7 @@ import org.seasar.doma.GeneratedValue;
 import org.seasar.doma.GenerationType;
 import org.seasar.doma.Id;
 import org.seasar.doma.SequenceGenerator;
+import org.seasar.doma.Version;
 
 @Entity
 public class Employee {
@@ -18,6 +19,7 @@ public class Employee {
 
     public Integer age;
 
+    @Version
     public Integer version;
 
     @Override
